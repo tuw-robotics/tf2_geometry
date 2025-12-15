f90f4b7cacffe467fda655061062109b81917b14
Markus Bader <markus.bader@tuwien.ac.at>
2025-12-15 12:46:29 +0100

    rosbridge added but empty

---
+++ include/tf2_geometry/ros.hpp
@@ -0,0 +1,13 @@
+#ifndef Tf2_GEOMETRY__ROS_HPP
+#define Tf2_GEOMETRY__ROS_HPP
+
+
+#include "tf2_geometry/transform2d.hpp"
+
+namespace tf2
+{
+  
+
+}  // namespace tf2
+
+#endif  // Tf2_GEOMETRY__ROS_HPP
